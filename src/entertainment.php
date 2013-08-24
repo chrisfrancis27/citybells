769085842e1579da31625abf8f742d75e6d97199
Chris Francis <chris@twoandahalfpeople.co.uk>
2013-08-24 12:11:10 +0100

    updated doctypes to html5, removed cruft

--- src/entertainment.php
+++ src/entertainment.php
@@ -1,6 +1,6 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head profile="http://gmpg.org/xfn/11">
+<!DOCTYPE html>
+<html>
+<head>
 
 	<title>The City Bells - London Saxophone Quartet & Horn Section - Bio</title>
 
